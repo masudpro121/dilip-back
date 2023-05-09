ef9a4dc446b6dbb02db57e28c0f953ff1dec76ed
masudpro121 <masud.pro.121@gmail.com>
2023-05-09 20:18:25 +0600

    generate header

--- routes/podcasts.js
+++ routes/podcasts.js
@@ -6,12 +6,21 @@ const axios = require('axios');
 
 const apiKey = "WMCG87GQJE2ESTMPPWWC";
 const apiSecret = "HfJ$QQwJ9hn2aBHKR9Gn$#2WTv8kEkCYfHFqeAv2";
-const apiHeaderTime = Math.floor(Date.now()/1000);
-const sha1Algorithm = "sha1";
-const data4Hash = apiKey + apiSecret + apiHeaderTime;
-const sha1Hash = crypto.createHash(sha1Algorithm);
-sha1Hash.update(data4Hash);
-const hash4Header = sha1Hash.digest('hex');
+
+const generateHeader = () => {
+  const apiHeaderTime = Math.floor(Date.now()/1000);
+  const sha1Algorithm = "sha1";
+  const data4Hash = apiKey + apiSecret + apiHeaderTime;
+  const sha1Hash = crypto.createHash(sha1Algorithm);
+  sha1Hash.update(data4Hash);
+  const hash4Header = sha1Hash.digest('hex');
+  return {
+    'X-Auth-Date': '' + apiHeaderTime,
+    'X-Auth-Key': apiKey,
+    'Authorization': hash4Header,
+    'User-Agent': 'SuperPodcastPlayer/1.8',
+  }
+}
 
 
 
@@ -19,50 +28,25 @@ const hash4Header = sha1Hash.digest('hex');
 PodcastRoute.get('/all', (req, res)=>{
     const options = {
         method: 'get',
-        headers: {
-          'X-Auth-Date': '' + apiHeaderTime,
-          'X-Auth-Key': apiKey,
-          'Authorization': hash4Header,
-          'User-Agent': 'SuperPodcastPlayer/1.8',
-        },
+        headers: generateHeader()
       };
     const url = `https://api.podcastindex.org/api/1.0/podcasts/trending?pretty`;
     axios(url, options)
     .then(response => {
       const result = response.data;
       res.send({status: 'ok', data: result});
-    });
+    })
+    .catch(err=>{
+      console.log(err, 'error');
+    })
 })
 
-// Podcast Transcribe
-PodcastRoute.get('/transcribe/:transurl', (req, res)=>{
-    const options = {
-        method: 'get',
-        headers: {
-          'X-Auth-Date': '' + apiHeaderTime,
-          'X-Auth-Key': apiKey,
-          'Authorization': hash4Header,
-          'User-Agent': 'SuperPodcastPlayer/1.8',
-        },
-      };
-    const url = req.params.transurl.replaceAll('-', '/')
-    console.log(url, 'url');
-    axios(url, options)
-    .then(response => {
-      const result = response.data.replace(/^\d(.+)?/gm, '').replaceAll('\n\n', '\n')
-      res.send(result);
-    });
-})
 
+// Podcast Episodes by id
 PodcastRoute.get('/episodes/:id', (req, res)=>{
     const options = {
         method: 'get',
-        headers: {
-          'X-Auth-Date': '' + apiHeaderTime,
-          'X-Auth-Key': apiKey,
-          'Authorization': hash4Header,
-          'User-Agent': 'SuperPodcastPlayer/1.8',
-        },
+        headers: generateHeader()
       };
     const id= req.params.id
     const url = `https://api.podcastindex.org/api/1.0/episodes/byfeedid?id=${id}&pretty`;
@@ -72,5 +56,35 @@ PodcastRoute.get('/episodes/:id', (req, res)=>{
       res.send({status: 'ok', data: result});
     });
 })
+
+
+// Podcast Transcribe
+PodcastRoute.get('/transcribe/:transurl', (req, res)=>{
+  const options = {
+      method: 'get',
+      headers: generateHeader()
+    };
+  const url = req.params.transurl.replaceAll('-', '/')
+  console.log(url, 'url');
+  axios(url, options)
+  .then(response => {
+    const result = response.data.replace(/^\d(.+)?/gm, '').replaceAll('\n\n', '\n')
+    res.send(result);
+  });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = PodcastRoute
