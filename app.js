42675eb9f2330f248df2656e2e5619601f87d889
masudpro121 <masud.pro.121@gmail.com>
2023-05-10 11:41:08 +0600

    error handle

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const PodcastRoute = require('./routes/podcasts')
 const PORT = process.env.PORT || 8000
 const app = express()
-
+app.set('server.timeout', 6000000);
 app.use(cors())
 app.use(express.json())
 app.use('/podcast', PodcastRoute)
@@ -16,6 +16,22 @@ app.listen(PORT, ()=>{
 })
 
 
+process.on('uncaughtException', (err, origin) => {
+  console.error(`Caught exception: ${err}\nException origin: ${origin}`);
+});
+
+process.on('unhandledRejection', (err, promise) => {
+  console.error(`Unhandled rejection: ${err}\nPromise: ${promise}`);
+});
+
+// Graceful shutdown
+process.on('SIGINT', () => {
+  console.log('Shutting down server...');
+  server.close(() => {
+    console.log('Server shut down');
+    process.exit(0);
+  });
+});
 
 
 
--- routes/podcasts.js
+++ routes/podcasts.js
@@ -1,118 +1,122 @@
-const  express = require('express')
-const PodcastRoute = express.Router()
-const fs = require('fs')
-const crypto = require('crypto');
-const axios = require('axios');
+const express = require("express");
+const PodcastRoute = express.Router();
+const fs = require("fs");
+const crypto = require("crypto");
+const axios = require("axios");
 
 const apiKey = "WMCG87GQJE2ESTMPPWWC";
 const apiSecret = "HfJ$QQwJ9hn2aBHKR9Gn$#2WTv8kEkCYfHFqeAv2";
 
 const generateHeader = () => {
-  const apiHeaderTime = Math.floor(Date.now()/1000);
+  const apiHeaderTime = Math.floor(Date.now() / 1000);
   const sha1Algorithm = "sha1";
   const data4Hash = apiKey + apiSecret + apiHeaderTime;
   const sha1Hash = crypto.createHash(sha1Algorithm);
   sha1Hash.update(data4Hash);
-  const hash4Header = sha1Hash.digest('hex');
+  const hash4Header = sha1Hash.digest("hex");
   return {
-    'X-Auth-Date': '' + apiHeaderTime,
-    'X-Auth-Key': apiKey,
-    'Authorization': hash4Header,
-    'User-Agent': 'SuperPodcastPlayer/1.8',
-  }
-}
+    "X-Auth-Date": "" + apiHeaderTime,
+    "X-Auth-Key": apiKey,
+    Authorization: hash4Header,
+    "User-Agent": "SuperPodcastPlayer/1.8",
+  };
+};
 
-
-
-// Get all Trending Podcast 
-PodcastRoute.get('/all', (req, res)=>{
+// Get all Trending Podcast
+PodcastRoute.get("/all", (req, res) => {
+  try{
     const options = {
-        method: 'get',
-        headers: generateHeader()
-      };
-    const url = `https://api.podcastindex.org/api/1.0/podcasts/trending?pretty`;
-    axios(url, options)
-    .then(response => {
+    method: "get",
+    headers: generateHeader(),
+  };
+  const url = `https://api.podcastindex.org/api/1.0/podcasts/trending?pretty`;
+  axios(url, options)
+    .then((response) => {
       const result = response.data;
-      res.send({status: 'ok', data: result});
+      res.send({ status: "ok", data: result });
     })
-    .catch(err=>{
-      console.log(err, 'error');
-    })
-})
-
-
+  }catch(err){
+    console.log(err.message);
+  }
+});
 
 // All Episodes by feed id
-PodcastRoute.get('/episodes/:id', (req, res)=>{
+PodcastRoute.get("/episodes/:id", (req, res) => {
+  try{
     const options = {
-        method: 'get',
-        headers: generateHeader()
-      };
-    const id= req.params.id
+      method: "get",
+      headers: generateHeader(),
+    };
+    const id = req.params.id;
     const url = `https://api.podcastindex.org/api/1.0/episodes/byfeedid?id=${id}&pretty`;
-    axios(url, options)
-    .then(response => {
+    axios(url, options).then((response) => {
       const result = response.data;
-      res.send({status: 'ok', data: result});
+      res.send({ status: "ok", data: result });
     });
-})
+  }catch(err){
+    console.log(err.message);
+  }
+});
+
 
 
 // 1 Episode by episodeid
-PodcastRoute.get('/episode/:feedId/:podcastId', async(req, res)=>{
-    const options = {
-        method: 'get',
-        headers: generateHeader()
-      };
-    const {feedId, podcastId}= req.params
-    const url = `https://api.podcastindex.org/api/1.0/episodes/byfeedid?id=${feedId}&pretty`;
-    const url2 = `https://api.podcastindex.org/api/1.0/podcasts/byfeedid?id=${feedId}&pretty`
-    let exactResult = {}
-    axios(url, options)
-    .then(response => {
-      const result = response.data.items;
-      exactResult = result.find(item=>item.id == podcastId)
-      axios(url2, options)
-      .then(response => {
-        const feed = response.data.feed;
-        exactResult.podcast = feed
-        res.send({status: 'ok', data: exactResult});
-      });
-      
-    });
-    
-   
-})
-
-
-// Podcast Transcribe
-PodcastRoute.get('/transcribe/:transurl', (req, res)=>{
-  const options = {
-      method: 'get',
-      headers: generateHeader()
+PodcastRoute.get("/episode/:feedId/:podcastId", async (req, res) => {
+  try{
+    const options1 = {
+      method: "get",
+      headers: generateHeader(),
     };
-  const url = req.params.transurl.replaceAll('-', '/')
-  console.log(url, 'url');
-  axios(url, options)
-  .then(response => {
-    const result = response.data.replace(/^\d(.+)?/gm, '').replaceAll('\n\n', '\n')
-    res.send(result);
-  });
-})
+    const { feedId, podcastId } = req.params;
+    const url = `https://api.podcastindex.org/api/1.0/episodes/byfeedid?id=${feedId}&pretty`;
+    const url2 = `https://api.podcastindex.org/api/1.0/podcasts/byfeedid?id=${feedId}&pretty`;
+    let exactResult = {};
+    axios(url, options1).then((response) => {
+      const result = response.data.items;
+      exactResult = result.find((item) => item.id == podcastId);
+      podcastAuthor(url2, exactResult, res)
+      
+    })
+    
+  }catch(err){
+    console.log(err.message);
+  }
+});
 
+// Function: Podcast Author
 
+const podcastAuthor = (url2, exactResult, res) => {
+  const options2 = {
+    method: "get",
+    headers: generateHeader(),
+  };
+  axios(url2, options2).then((response) => {
+    const feed = response.data.feed;
+    exactResult.podcast = feed;
+    
+    const url = exactResult.transcriptUrl;
+    if(url){
+      podcastTranscribe(url, exactResult, res)
+    }else{
+      res.send({ status: "ok", data: exactResult });
+    }
+  })
+}
 
+// Function: Podcast Transcribe
+const podcastTranscribe = (url, exactResult, res) => {
+  const options3 = {
+    method: "get",
+    headers: generateHeader(),
+  };
+  axios(url, options3).then((response) => {
+    const result = response.data
+      .replace(/^\d(.+)?/gm, "")
+      .replaceAll("\n", "");
+    exactResult.transcription = result;
+    res.send({ status: "ok", data: exactResult });
+  })
+  
+}
 
-
-
-
-
-
-
-
-
-
-
-
-module.exports = PodcastRoute
+module.exports = PodcastRoute;
